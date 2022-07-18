c83f0a3f64ea79e00ef728150e7ec956c88e2365
binghui-z <z_binghui@163.com>
2022-07-18 14:15:12 +0800

    add

--- test2.py
+++ test2.py
@@ -1,1 +1,9 @@
-print("Hello world")
+sum = 100+120
+sub = 120-100
+mul = 100*30
+div = 10/3
+print("main branch")
+print("sum is:%f"%(sum))
+print("sub is:%f"%(sub))
+print("mul is:%f"%(mul))
+print("sub is:%f"%(div))
